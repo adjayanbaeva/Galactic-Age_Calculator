93822d201eae7491d8d7a55920b1ca7fd246b429
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2018-03-16 21:54:26 -0700

    test method that converts human age into Venus years, test fails as expected

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -9,7 +9,13 @@ describe('Age Calculator', function() {
 
   it('should convert human age into Mercury years', function(){
     expect(newCalculator.GetAgeInMercuryYears(20)).toEqual(83);
-  })
+  });
+
+  it('should convert human age into Mercury years', function(){
+    expect(newCalculator.GetAgeInVenusYears(20)).toEqual(32);
+  });
+
+
 
   // it('should calculate the difference between birthdate and current date and show the result in seconds', function() {
   // expect(newCalculator.GetDifferenceBetweenDatesInSeconds())
